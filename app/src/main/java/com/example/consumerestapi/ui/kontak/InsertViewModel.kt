02891f1bcf3db37856e7de1f3ca3a662b86affef
Khalishah18 <khalishahsalsabilla18@gmail.com>
2023-12-28 19:00:51 +0700

    add suspend fun

--- app/src/main/java/com/example/consumerestapi/ui/kontak/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/kontak/InsertViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.consumerestapi.ui.kontak
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.consumerestapi.repository.KontakRepository
 import com.example.consumerestapi.ui.home.viewmodel.InsertUiEvent
 import com.example.consumerestapi.ui.home.viewmodel.InsertUiState
+import kotlinx.coroutines.launch
 
 class InsertViewModel (private val kontakRepository: KontakRepository) : ViewModel() {
 
@@ -12,5 +15,14 @@ class InsertViewModel (private val kontakRepository: KontakRepository) : ViewMod
         private set
     fun updateInsertKontakState(insertUiEvent: InsertUiEvent){
         insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 }
